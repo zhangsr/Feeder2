612abc8139ef87b33ad019e94417099d444db64f
match <zsrscut@gmail.com>
2016-10-23 14:40:33 +0800

    [Feature] Subscription icon load

--- app/src/main/java/com/feeder/android/utils/SubscriptionViewHolder.java
+++ app/src/main/java/com/feeder/android/utils/SubscriptionViewHolder.java
@@ -3,7 +3,9 @@ package com.feeder.android.utils;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.volley.toolbox.NetworkImageView;
 import com.bignerdranch.expandablerecyclerview.ViewHolder.ChildViewHolder;
+import com.feeder.domain.VolleySingleton;
 import com.feeder.model.Subscription;
 
 import me.zsr.feeder.R;
@@ -16,20 +18,28 @@ import me.zsr.feeder.R;
 public class SubscriptionViewHolder extends ChildViewHolder {
     private View mItemView;
     private TextView mNameTextView;
+    private NetworkImageView mIconImageView;
 
     public SubscriptionViewHolder(View itemView) {
         super(itemView);
         mItemView = itemView;
         mNameTextView = (TextView) itemView.findViewById(R.id.subscription_name);
+        mIconImageView = (NetworkImageView) itemView.findViewById(R.id.subscription_icon);
+        mIconImageView.setDefaultImageResId(R.drawable.ic_rss_feed_black_24dp);
+        mIconImageView.setErrorImageResId(R.drawable.ic_rss_feed_black_24dp);
     }
 
     public void bind(Subscription subscription, final View.OnClickListener listener) {
-        mNameTextView.setText(subscription.getTitle());
+        if (subscription == null) {
+            return;
+        }
         mItemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 listener.onClick(v);
             }
         });
+        mNameTextView.setText(subscription.getTitle());
+        mIconImageView.setImageUrl(subscription.getIconUrl(), VolleySingleton.getInstance().getImageLoader());
     }
 }
