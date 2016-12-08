40ca81a6f70111e35349e32c906a5d4417be2245
match <zsrscut@gmail.com>
2016-12-08 23:18:50 +0800

    [Feature] Share

---
+++ app/src/main/java/com/feeder/android/util/ShareHelper.java
@@ -0,0 +1,125 @@
+package com.feeder.android.util;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.Toast;
+
+import com.feeder.model.Article;
+
+import java.util.HashMap;
+
+import cn.sharesdk.framework.Platform;
+import cn.sharesdk.framework.PlatformActionListener;
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.instapaper.Instapaper;
+import cn.sharesdk.sina.weibo.SinaWeibo;
+import cn.sharesdk.wechat.friends.Wechat;
+import cn.sharesdk.wechat.moments.WechatMoments;
+import me.zsr.feeder.R;
+
+/**
+ * @description:
+ * @author: Match
+ * @date: 12/7/16
+ */
+
+public class ShareHelper {
+    private Activity mActivity;
+    private Bitmap mDefaultBitmap;
+
+    public ShareHelper(Activity activity) {
+        mActivity = activity;
+        mDefaultBitmap = BitmapFactory.decodeResource(activity.getResources(), R.drawable.ic_launcher);
+    }
+
+    private PlatformActionListener mPlatformActionListener = new PlatformActionListener() {
+        @Override
+        public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+            showToast(mActivity.getString(R.string.share_complete));
+        }
+
+        @Override
+        public void onError(Platform platform, int i, Throwable throwable) {
+            showToast(mActivity.getString(R.string.share_failed));
+        }
+
+        @Override
+        public void onCancel(Platform platform, int i) {
+            showToast(mActivity.getString(R.string.share_cancel));
+        }
+    };
+
+    public void shareToWechat(Article article) {
+        Wechat.ShareParams sp = new Wechat.ShareParams();
+        sp.setShareType(Platform.SHARE_WEBPAGE);
+        sp.setTitle(article.getTitle());
+        sp.setText(article.getDescription());
+        sp.setUrl(article.getLink());
+        // TODO: 12/7/16 how to get source
+//        FeedSource feedSource = mFeedItem.getFeedSource();
+//        if (feedSource != null
+//                && !TextUtils.isEmpty(feedSource.getFavicon())) {
+//            sp.setImageUrl(feedSource.getFavicon());
+//        } else if (mDefaultBitmap != null) {
+//            sp.setImageData(mDefaultBitmap);
+//        }
+        sp.setImageData(mDefaultBitmap);
+
+        Platform platform = ShareSDK.getPlatform(Wechat.NAME);
+        platform.setPlatformActionListener(mPlatformActionListener);
+        platform.share(sp);
+    }
+
+    public void shareToMoment(Article article) {
+        WechatMoments.ShareParams sp = new WechatMoments.ShareParams();
+        sp.setShareType(Wechat.SHARE_WEBPAGE);
+        sp.setTitle(article.getTitle());
+        sp.setText(article.getDescription());
+        sp.setUrl(article.getLink());
+//        FeedSource feedSource = mFeedItem.getFeedSource();
+//        if (feedSource != null
+//                && !TextUtils.isEmpty(feedSource.getFavicon())) {
+//            sp.setImageUrl(feedSource.getFavicon());
+//        } else if (mDefaultBitmap != null) {
+//            sp.setImageData(mDefaultBitmap);
+//        }
+        sp.setImageData(mDefaultBitmap);
+
+        Platform platform = ShareSDK.getPlatform(WechatMoments.NAME);
+        platform.setPlatformActionListener(mPlatformActionListener);
+        platform.share(sp);
+    }
+
+    public void shareToWeibo(Article article) {
+        SinaWeibo.ShareParams sp = new SinaWeibo.ShareParams();
+        sp.setText(getPureTextShare(article));
+
+        Platform platform = ShareSDK.getPlatform(SinaWeibo.NAME);
+        platform.setPlatformActionListener(mPlatformActionListener);
+        platform.share(sp);
+    }
+
+    public void shareToInstapaper(Article article) {
+        Instapaper.ShareParams sp = new Instapaper.ShareParams();
+        sp.setTitle(article.getTitle());
+        sp.setText(article.getDescription());
+        sp.setUrl(article.getLink());
+
+        Platform platform = ShareSDK.getPlatform(Instapaper.NAME);
+        platform.setPlatformActionListener(mPlatformActionListener);
+        platform.share(sp);
+    }
+
+    private String getPureTextShare(Article article) {
+        String shareText = article.getTitle()
+                + " "
+                + article.getLink()
+                + " Shared by Feeder app";
+        return shareText;
+    }
+
+    private void showToast(String toast) {
+        Toast.makeText(mActivity, toast, Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/feeder/android/view/article/ArticleActivity.java
+++ app/src/main/java/com/feeder/android/view/article/ArticleActivity.java
@@ -1,11 +1,19 @@
 package com.feeder.android.view.article;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.simplelist.MaterialSimpleListAdapter;
+import com.afollestad.materialdialogs.simplelist.MaterialSimpleListItem;
 import com.feeder.android.util.Constants;
 import com.feeder.android.util.DateUtil;
+import com.feeder.android.util.ShareHelper;
 import com.feeder.android.view.BaseSwipeActivity;
 import com.feeder.android.view.SettingsActivity;
 import com.feeder.common.SPManager;
@@ -20,8 +28,10 @@ import com.google.common.base.Strings;
 import org.sufficientlysecure.htmltextview.HtmlHttpImageGetter;
 import org.sufficientlysecure.htmltextview.HtmlTextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import cn.sharesdk.framework.ShareSDK;
 import me.zsr.feeder.R;
 
 /**
@@ -33,11 +43,14 @@ import me.zsr.feeder.R;
 // TODO: 10/31/16 optimize : make image match parent``
 // TODO: 10/30/16 to be modularity
 public class ArticleActivity extends BaseSwipeActivity {
+    private Toolbar mToolbar;
     private HtmlTextView mContentTextView;
     private TextView mTitleTextView;
     private TextView mSubscriptionNameTextView;
     private TextView mDateTextView;
     private TextView mTimeTextView;
+    private ShareHelper mShareHelper;
+    private Article mArticle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +60,26 @@ public class ArticleActivity extends BaseSwipeActivity {
         initViews();
         Long articleId = getIntent().getExtras().getLong(Constants.KEY_BUNDLE_ARTICLE_ID);
         loadDataAsync(articleId);
+
+        ShareSDK.initSDK(this);
+        mShareHelper = new ShareHelper(this);
     }
 
     private void initViews() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.inflateMenu(R.menu.menu_article);
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_share:
+                        showShareMenu();
+                        break;
+                }
+                return false;
+            }
+        });
+
         mTitleTextView = (TextView) findViewById(R.id.article_title);
         mDateTextView = (TextView) findViewById(R.id.article_date);
         mTimeTextView = (TextView) findViewById(R.id.article_time);
@@ -82,10 +112,10 @@ public class ArticleActivity extends BaseSwipeActivity {
                 if (articleList.size() != 1) {
                     return;
                 }
-                final Article article = articleList.get(0);
+                mArticle = articleList.get(0);
 
                 final List<Subscription> subscriptionList = DBManager.getSubscriptionDao().queryBuilder().where(
-                        SubscriptionDao.Properties.Id.eq(article.getSubscriptionId())).list();
+                        SubscriptionDao.Properties.Id.eq(mArticle.getSubscriptionId())).list();
                 if (subscriptionList.size() != 1) {
                     return;
                 }
@@ -93,7 +123,7 @@ public class ArticleActivity extends BaseSwipeActivity {
                 ThreadManager.post(new Runnable() {
                     @Override
                     public void run() {
-                        setData(article, subscriptionList.get(0).getTitle());
+                        setData(mArticle, subscriptionList.get(0).getTitle());
                     }
                 });
             }
@@ -118,5 +148,66 @@ public class ArticleActivity extends BaseSwipeActivity {
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(R.anim.right_in, R.anim.right_out);
+    }
+
+    private void showShareMenu() {
+        final MaterialSimpleListAdapter adapter = new MaterialSimpleListAdapter(this);
+        final List<Integer> contentIdList = new ArrayList<>();
+        if (SPManager.getBoolean(SettingsActivity.KEY_SWITCH_SHARE_WECHAT, true)) {
+            adapter.add(new MaterialSimpleListItem.Builder(this)
+                    .content(R.string.wechat)
+                    .icon(R.drawable.ic_menu_wechat)
+                    .backgroundColor(Color.WHITE)
+                    .build());
+            contentIdList.add(R.string.wechat);
+        }
+        if (SPManager.getBoolean(SettingsActivity.KEY_SWITCH_SHARE_MOMENT, true)) {
+            adapter.add(new MaterialSimpleListItem.Builder(this)
+                    .content(R.string.moment)
+                    .icon(R.drawable.ic_menu_moment)
+                    .backgroundColor(Color.WHITE)
+                    .build());
+            contentIdList.add(R.string.moment);
+        }
+        if (SPManager.getBoolean(SettingsActivity.KEY_SWITCH_SHARE_WEIBO, true)) {
+            adapter.add(new MaterialSimpleListItem.Builder(this)
+                    .content(R.string.weibo)
+                    .icon(R.drawable.ic_menu_weibo)
+                    .backgroundColor(Color.WHITE)
+                    .build());
+            contentIdList.add(R.string.weibo);
+        }
+        if (SPManager.getBoolean(SettingsActivity.KEY_SWITCH_SHARE_INSTAPAPER, true)) {
+            adapter.add(new MaterialSimpleListItem.Builder(this)
+                    .content(R.string.instapaper)
+                    .icon(R.drawable.ic_menu_instapaper)
+                    .backgroundColor(Color.WHITE)
+                    .build());
+            contentIdList.add(R.string.instapaper);
+        }
+
+        new MaterialDialog.Builder(this)
+                .title(R.string.share_to)
+                .adapter(adapter, new MaterialDialog.ListCallback() {
+                    @Override
+                    public void onSelection(MaterialDialog dialog, View itemView, int which, CharSequence text) {
+                        switch (contentIdList.get(which)) {
+                            case R.string.wechat:
+                                mShareHelper.shareToWechat(mArticle);
+                                break;
+                            case R.string.moment:
+                                mShareHelper.shareToMoment(mArticle);
+                                break;
+                            case R.string.weibo:
+                                mShareHelper.shareToWeibo(mArticle);
+                                break;
+                            case R.string.instapaper:
+                                mShareHelper.shareToInstapaper(mArticle);
+                                break;
+                        }
+                        dialog.dismiss();
+                    }
+                })
+                .show();
     }
 }
