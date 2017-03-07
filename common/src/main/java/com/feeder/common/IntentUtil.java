5f8d284aeccd36ac03a16870bb88c987b9fdcf01
match <zsrscut@gmail.com>
2017-03-07 22:19:12 +0800

    [Bugfix] Handle activity not found exception

--- common/src/main/java/com/feeder/common/IntentUtil.java
+++ common/src/main/java/com/feeder/common/IntentUtil.java
@@ -1,6 +1,7 @@
 package com.feeder.common;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 
@@ -13,9 +14,13 @@ import android.net.Uri;
 public class IntentUtil {
 
     public static void openUrl(Activity activity, String url) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse(url));
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        activity.startActivity(intent);
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            activity.startActivity(intent);
+        } catch (ActivityNotFoundException e) {
+            // TODO: 3/7/17  tips or ?
+        }
     }
 }
